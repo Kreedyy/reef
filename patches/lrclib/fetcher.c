96ca47f53961730e0d64b3340431341ec8190163
Kreedy <kreedy@noreply.localhost>
2026-08-23 19:14:43 +0200

    [patch/lrclib] Swap track and artist in url query

--- patches/lrclib/fetcher.c
+++ patches/lrclib/fetcher.c
@@ -163,7 +163,7 @@ lrclib_provider(const char *artist, const char *title) {
     return;
   }
 
-  snprintf(url, sizeof(url), "https://lrclib.net/api/search?q=%s+%s", t, a);
+  snprintf(url, sizeof(url), "https://lrclib.net/api/search?q=%s+%s", a, t);
   http_escape_free(a);
   http_escape_free(t);
 
